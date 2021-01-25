933951fafdb2b3db62737c09f724c081c7dc97ba
Celesica <j.enelya13@gmail.com>
2021-01-25 15:24:01 +0800

    init

---
+++ src/Lab1/OutputText.java
@@ -0,0 +1,9 @@
+package Lab1;
+
+public class OutputText {
+    public static void main(String[] args) {
+        System.out.println("\"I am writing a program that will display \"text\" with escape characters.");
+        System.out.println("\t At the beginning of this sentence it has a tab escape character.");
+        System.out.println("\nThere are two new lines before this sentence and you also have to include 'single\nquote' and \\back slash\\ characters in this sentence.");
+    }
+}
